d59e83c300947fa01cd0caf6bbf3aee7bf37b27e
Sleepy105 <lm.sousa@fe.up.pt>
2021-04-16 15:05:45 +0100

    Add documentation

--- src/HoeffdingTree.hpp
+++ src/HoeffdingTree.hpp
@@ -18,7 +18,24 @@ class HoeffdingTree : public BinaryTree {
         uint _sampleCount = 0;
         static const uint _nAttributes = HT_DATA_ATTRIBUTES_N;
 
+        /**
+         * @brief Calculates the Hoeffding Bound
+         *
+         * @param r Range of variable
+         * @param sigma acceptable error margin (0.0 to 1.0)
+         * @param n_samples Number of samples in the leaf node
+         * @return float Hoeffding bound
+         */
         static float _hoeffdingBound(uint r, uint sigma, uint n_samples);
+
+        /**
+         * @brief Cumulative Moving Average
+         *
+         * @param CMA_n Current CMA value
+         * @param n_1 New sample count
+         * @param x_n_1 New sample
+         * @return constexpr float New CMA value
+         */
         constexpr float _CMA(float CMA_n, uint n_1, float x_n_1);
     };
 
