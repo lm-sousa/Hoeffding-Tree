2f68068cd4cbe148bec9ff651925eddce9b77cff
Sleepy105 <lm.sousa@fe.up.pt>
2021-05-25 15:16:19 +0100

    Quantile update algorithm

--- src/HoeffdingTree.hpp
+++ src/HoeffdingTree.hpp
@@ -15,21 +15,10 @@ template <class Data> class HoeffdingTree : public BinaryTree<Node<Data>> {
      * @param r Range of variables
      * @param sigma acceptable error margin (0.0 to 1.0)
      */
-    HoeffdingTree<Data>(uint r, float sigma)
+    HoeffdingTree(uint r, float sigma)
         : _hoeffdingBoundConstant(r * (sqrt(-log(sigma) / 2))) {}
 
     void train(typename Data::datatype data[]);
-
-    /**
-     * @brief Asymmetric signum function
-     *
-     * @param z
-     * @param alpha
-     * @return constexpr uint
-     */
-    constexpr uint sgnAlpha(typename Data::datatype z, float alpha) {
-        return z < 0 ? -alpha : 1 - alpha;
-    }
 
     /**
      * @brief Calculates the Hoeffding Bound
--- src/NodeData.hpp
+++ src/NodeData.hpp
@@ -4,32 +4,54 @@
 #include <stdlib.h>
 #include <sys/types.h>
 
-template <typename _datatype = float, uint N_Attributes = 1,
-          uint N_Quantiles = 1>
+template <typename _datatype = float, uint N_Attributes = 16,
+          uint N_Classes = 2, uint N_Quantiles = 8>
 class NodeData {
   public:
     typedef _datatype datatype;
-    void update(datatype data[]) {
-        _sampleCount++;
-        bool homogeneous = false;
+
+    NodeData(float alpha = 0, float lambda = 0.01)
+        : _alpha(alpha), _lambda(lambda) {}
+
+    /**
+     * @brief Asymmetric signum function
+     *
+     * @param z
+     * @return constexpr uint
+     */
+    constexpr uint sgnAlpha(datatype z) { return z < 0 ? -_alpha : 1 - _alpha; }
+
+    void update(datatype data[], uint classification) {
+
+        _sampleCountTotal++;
+        _sampleCountPerClass[classification]++;
 
         for (uint i = 0; i < N_Attributes; i++) {
-            _attributesCount[i] += data[i];
-            if (_attributesCount[i] == _sampleCount) {
-                homogeneous = true;
+            if (data[i] < _attributeRanges[i][AttibuteRange::Min]) {
+                _attributeRanges[i][AttibuteRange::Min] = data[i];
             }
-        }
 
-        if (!homogeneous) {
-            // calculate G for every attribute
-            // Calculate _hoeffdingBound()
+            if (data[i] > _attributeRanges[i][AttibuteRange::Max]) {
+                _attributeRanges[i][AttibuteRange::Max] = data[i];
+            }
+
+            for (uint k = 0; k < N_Quantiles; k++) {
+                _Attributes[i][classification][k] -=
+                    _lambda *
+                    sgnAlpha(_Attributes[i][classification][k] - data[i]);
+            }
         }
     }
 
+    enum AttibuteRange { Min = 0, Max = 1 };
+
   protected:
-    uint _attributesCount[N_Attributes] = {0};
-    uint _sampleCount = 0;
-    datatype _Attributes[N_Attributes][N_Quantiles] = {0};
+    uint _sampleCountTotal = 0;
+    uint _sampleCountPerClass[N_Classes] = {0};
+    datatype _Attributes[N_Attributes][N_Classes][N_Quantiles] = {0};
+    datatype _attributeRanges[N_Attributes][2] = {0};
+
+    const float _alpha, _lambda;
 };
 
 #endif
