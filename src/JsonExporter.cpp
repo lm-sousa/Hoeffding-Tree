4d45f40570a134ce7f72436242b3b561f466dd4e
Sleepy105 <lm.sousa@fe.up.pt>
2021-06-10 01:57:12 +0100

    Generatr 'tree_' field of json export

---
+++ src/JsonExporter.cpp
@@ -0,0 +1,12 @@
+#include "JsonExporter.hpp"
+
+const std::string JsonExporter::dtypes =
+    "[\"<i8\",\"<i8\",\"<i8\",\"<f8\",\"<f8\",\"<i8\",\"<f8\"]";
+
+const std::string JsonExporter::params =
+    "{\"ccp_alpha\": 0.0,\"class_weight\": null,\"criterion\": "
+    "\"gini\",\"max_depth\": null,\"max_features\": "
+    "null,\"max_leaf_nodes\": null,\"min_impurity_decrease\": "
+    "0.0,\"min_impurity_split\": null,\"min_samples_leaf\": "
+    "1,\"min_samples_split\": 2,\"min_weight_fraction_leaf\": "
+    "0.0,\"random_state\": null,\"splitter\": \"best\"}";
--- src/JsonExporter.hpp
+++ src/JsonExporter.hpp
@@ -84,8 +84,20 @@ class JsonExporter {
             values.push_back(pair.second.second);
         }
 
-        std::cout << "Nodes: " << vectorToJson(nodes) << std::endl;
-        std::cout << "Values: " << vectorToJson(values) << std::endl;
+        std::map<std::string, std::string> tree_;
+
+        tree_.insert(std::pair<std::string, std::string>(
+            std::string("max_depth"), std::to_string(nodeMap.size())));
+        tree_.insert(std::pair<std::string, std::string>(
+            std::string("node_count"), std::to_string(nodeMap.size())));
+        tree_.insert(std::pair<std::string, std::string>(std::string("nodes"),
+                                                         vectorToJson(nodes)));
+        tree_.insert(std::pair<std::string, std::string>(std::string("values"),
+                                                         vectorToJson(values)));
+        tree_.insert(std::pair<std::string, std::string>(
+            std::string("nodes_dtype"), dtypes));
+
+        std::cout << "tree_: " << mapToJson(tree_) << std::endl;
 
         return "";
     }
@@ -168,16 +180,8 @@ class JsonExporter {
     static const char objCharEnd = '}';
     static const char objCharDelimiter = ',';
 
-    const std::string dtypes =
-        "[\"<i8\",\"<i8\",\"<i8\",\"<f8\",\"<f8\",\"<i8\",\"<f8\"]";
-
-    const std::string params =
-        "{\"ccp_alpha\": 0.0,\"class_weight\": null,\"criterion\": "
-        "\"gini\",\"max_depth\": null,\"max_features\": "
-        "null,\"max_leaf_nodes\": null,\"min_impurity_decrease\": "
-        "0.0,\"min_impurity_split\": null,\"min_samples_leaf\": "
-        "1,\"min_samples_split\": 2,\"min_weight_fraction_leaf\": "
-        "0.0,\"random_state\": null,\"splitter\": \"best\"}";
+    static const std::string dtypes;
+    static const std::string params;
 
     template <class T, class fn_T>
     static void _DFS_handle(T *node, fn_T function, uint &nodeCounter) {
