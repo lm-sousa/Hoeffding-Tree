c8725356f95b54e6b07938c02fe818623b1d5cca
Sleepy105 <lm.sousa@fe.up.pt>
2021-06-16 16:37:32 +0100

    Update BinaryTree to use node indexes instead of pointers

--- lib/BinaryTree.hpp
+++ lib/BinaryTree.hpp
@@ -12,51 +12,57 @@ template <class Node, uint8_t capacity = 100> class BinaryTree {
 
   public:
     typedef Node _NodeClass;
-    typedef typename Node::_DataClass::datatype datatype;
+    typedef typename _NodeClass::_DataClass _DataClass;
+    typedef typename _NodeClass::node_index_t node_index_t;
+    typedef typename _DataClass::datatype datatype;
 
-    constexpr uint8_t getCapacity() { return capacity; }
-    uint8_t getSize() { return _size; }
+    constexpr node_index_t getCapacity() { return capacity; }
+    node_index_t getSize() { return _size; }
     void increaseSize() { _size++; }
     bool canAddNode() { return _size < capacity; }
 
-    Node *getRootNode() { return &_nodes[0]; }
+    Node &getNode(node_index_t index) { return _nodes[index]; }
 
-    Node *addLeftChild(Node *node) {
+    Node &getRootNode() { return getNode(getRootNodeIndex()); }
 
-        Node *newNode = _getNextFreeNode();
-        if (!newNode) {
-            return NULL;
+    node_index_t getRootNodeIndex() { return 0; }
+
+    node_index_t addLeftChild(Node &node) {
+
+        node_index_t newNodeIndex = _getNextFreeNode();
+        if (newNodeIndex == -1) {
+            return -1;
         }
 
-        node->setLeftChild(newNode);
+        node.setLeftChild(newNodeIndex);
         this->increaseSize();
 
-        return newNode;
+        return newNodeIndex;
     }
 
-    Node *addRightChild(Node *node) {
-        Node *newNode = _getNextFreeNode();
-        if (!newNode) {
-            return NULL;
+    node_index_t addRightChild(Node &node) {
+        node_index_t newNodeIndex = _getNextFreeNode();
+        if (newNodeIndex == -1) {
+            return -1;
         }
 
-        node->setRightChild(newNode);
+        node.setRightChild(newNodeIndex);
         this->increaseSize();
 
-        return newNode;
+        return newNodeIndex;
     }
 
-    Node *sortSample(datatype x[]) {
-        Node *node = getRootNode();
-        for (Node *newNode = node; newNode != NULL;
-             newNode = node->sortSample(x)) {
+    node_index_t sortSample(datatype x[]) {
+        node_index_t node = getRootNodeIndex();
+        for (node_index_t newNode = node; newNode != -1;
+             newNode = getNode(node).sortSample(x)) {
             node = newNode;
         }
         return node;
     }
 
-    void splitNode(Node *node, uint attributeIndex, datatype splitValue) {
-        node->setSplit(attributeIndex, splitValue);
+    void splitNode(Node &node, uint attributeIndex, datatype splitValue) {
+        node.setSplit(attributeIndex, splitValue);
         this->addLeftChild(node);
         this->addRightChild(node);
     }
@@ -65,7 +71,13 @@ template <class Node, uint8_t capacity = 100> class BinaryTree {
     uint8_t _size = 1;
     Node _nodes[capacity];
 
-    Node *_getNextFreeNode() { return &_nodes[getSize()]; }
+    node_index_t _getNextFreeNode() {
+        if (getSize() >= getCapacity()) {
+            return -1;
+        } else {
+            return getSize();
+        }
+    }
 };
 
 #endif
