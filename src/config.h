bd4f51a477bb40f209062559573ee46e3ed344b8
L. Sousa <lm.sousa@fe.up.pt>
2023-03-29 23:59:52 +0100

    I don't know what to call this

---
+++ src/config.h
@@ -0,0 +1,35 @@
+#ifndef __HOEFFDING_TREE_CONFIG_HPP__
+#define __HOEFFDING_TREE_CONFIG_HPP__
+
+// #define USE_XILINX_AP_TYPES
+
+#include "HoeffdingTree.hpp"
+
+#define DATATYPE float
+#define ATTRIBUTES 4
+#define CLASSES 3
+#define NODES 127
+#define SAMPLE_ARRAY_SIZE 150
+
+typedef HoeffdingTree<Node<NodeData<DATATYPE,   // datatype
+                                    ATTRIBUTES, // Attributes
+                                    CLASSES     // Classes
+                                    >,
+                           NODES // Node upperbound
+                           >>
+    Tree;
+
+typedef Tree::data_t data_t;
+
+template <class T> struct Prediction {
+    typename T::class_index_t classification;
+    typename T::data_t confidence;
+};
+
+template <class T> struct Sample {
+    typename T::data_t data[T::_DataClass::N_Attributes];
+    typename T::class_index_t classification;
+    bool doSplitTrial;
+};
+
+#endif // __HOEFFDING_TREE_CONFIG_HPP__
---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+#include "config.h"
+
+//extern "C" {
+
+void krnl_Tree(Tree *tree, Sample<Tree> sample[SAMPLE_ARRAY_SIZE],
+               Prediction<Tree> prediction[SAMPLE_ARRAY_SIZE],
+               unsigned int bundleSize) {
+
+sample_loop:
+    for (unsigned int i = 0; i < bundleSize; i++) {
+        if (sample[i].doSplitTrial) {
+            std::tie(prediction[i].classification, prediction[i].confidence) =
+                tree->train(sample[i].data, sample[i].classification, true);
+        } else {
+            std::tie(prediction[i].classification, prediction[i].confidence) =
+                tree->infer(sample[i].data);
+        }
+    }
+}
+//}
